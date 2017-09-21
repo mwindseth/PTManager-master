62329de3b97e3336e845c0ab357178a2478f37c9
mwindseth <31023587+mwindseth@users.noreply.github.com>
2017-09-20 21:59:13 -0400

    Added: Menu -> Log Out

--- app/src/main/java/windseth/matthew/ptcustomermanagement/MainActivity.java
+++ app/src/main/java/windseth/matthew/ptcustomermanagement/MainActivity.java
@@ -1,9 +1,14 @@
 package windseth.matthew.ptcustomermanagement;
 
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,5 +20,23 @@ public class MainActivity extends AppCompatActivity {
         // Load Fragment
         FragmentManager fm = getSupportFragmentManager();
 
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id=item.getItemId();
+        if(id==R.id.log_off){
+            Intent logout = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(logout);
+            Toast.makeText(MainActivity.this,R.string.logging_off,Toast.LENGTH_SHORT).show();
+            this.finish();
+            return true;
+        }
+        return true;
     }
 }
